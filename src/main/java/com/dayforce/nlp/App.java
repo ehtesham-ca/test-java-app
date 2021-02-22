0439422bf7a9e446932f2cd21ac494d42910e665
Choudhury <ehtesham.choudhury@ceridian.com>
2021-02-22 14:50:54 -0500

    initial commit

---
+++ src/main/java/com/dayforce/nlp/App.java
@@ -0,0 +1,13 @@
+package com.dayforce.nlp;
+
+/**
+ * Application starting point
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Dayforce NLP Core" );
+    }
+}
